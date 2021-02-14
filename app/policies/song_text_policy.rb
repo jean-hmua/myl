93138d94ef79821a61faf39665bf56a301446708
Jean Battirola <jeanbattirola@gmail.com>
2021-02-14 21:37:41 +0100

    check diff

--- app/policies/song_text_policy.rb
+++ app/policies/song_text_policy.rb
@@ -3,38 +3,38 @@ class SongTextPolicy < ApplicationPolicy
     def resolve
       scope.where(user: user)
     end
+  end
+  
+  def index?
+    true
+    record.user == user || user.admin?
+  end
 
-    def index?
-      true
-      # record.user == user || user.admin?
-    end
-  
-    def show?
-      true
-      # record.user == user || user.admin?
-    end
-  
-    def create?
-      true
-      # record.user == user || user.admin?
-    end
-  
-    def new?
-      create?
-    end
-  
-    def update?
-      true
-      # record.user == user || user.admin?
-    end
-  
-    def edit?
-      update?
-    end
-  
-    def destroy?
-      true
-      # record.user == user || user.admin?
-    end
+  def show?
+    true
+    record.user == user || user.admin?
+  end
+
+  def create?
+    true
+    record.user == user || user.admin?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    true
+    record.user == user || user.admin?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    true
+    record.user == user || user.admin?
   end
 end
