0a1be86aa095de08eadd738fd895f7cef8da84ca
Jean Battirola <jeanbattirola@gmail.com>
2021-03-15 16:38:50 +0100

    basic layout finally done

--- app/controllers/song_texts_controller.rb
+++ app/controllers/song_texts_controller.rb
@@ -1,6 +1,5 @@
 class SongTextsController < ApplicationController
   before_action :set_song_text, only: %i[ show edit update destroy ]
-  before_action :get_page_to_redirect
   before_action :authenticate_user!
 
   # GET /song_texts or /song_texts.json
@@ -10,8 +9,10 @@ class SongTextsController < ApplicationController
 
   # USER_SONG_TEXTS
 def user_song_texts
+  set_page_to_redirect
   @user = current_user
-  @song_texts = policy_scope(SongText).order(created_at: :desc)
+  @song_text = current_user.song_texts.new
+  @song_texts = SongText.where(user_id: @user)
   authorize @song_texts
 end
 
@@ -37,10 +38,10 @@ end
     @song_text = current_user.song_texts.new(song_text_params)
     authorize @song_text
       if @song_text.save
-        redirect_to session.delete(:return_to)
+        redirect_to user_song_texts_path
         flash[:notice] = " \n #{@song_text.title} was created"
       else
-        redirect_to session.delete(:return_to)
+        redirect_to user_song_texts_path
         flash[:notice] = " \n #{@song_text.title} was NOT created: #{@song_text.errors}"
       end
   end
@@ -85,7 +86,7 @@ end
       params.require(:song_text).permit(:title, :content, :notes, :rating, :color_tag, :created_at, :updated_at)
     end
 
-    def get_page_to_redirect
+    def set_page_to_redirect
       session[:return_to] ||= request.referer
     end
 
