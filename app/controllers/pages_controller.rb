730f20fd07686269a0473d444996b676988c0c10
Jean Battirola <jeanbattirola@gmail.com>
2021-03-30 10:40:06 +0200

    feature ready: public? function ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,11 @@ class PagesController < ApplicationController
 
   def profile
     @user = User.find(params[:id])
+    @song_texts = []
     
-    @song_texts = SongText.where(user: @user && public_lyric = true)
+    @user_song_texts = SongText.where(user: @user)
+    @user_song_texts.each do |s|
+      @song_texts << s if s.public_lyric == true
+    end
   end
 end
